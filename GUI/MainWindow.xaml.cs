3901b3007ceaf9d54f86f68a531c0a14f368929e
Abiel Kiflu <abiel.kiflu.weld@gmail.com>
2023-07-28 09:33:09 +0200

    Arduion interface

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -20,15 +20,55 @@ namespace GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int[] dataPoints = { 3, 5, 8, 4, 7, 2 };
         public MainWindow()
         {
             InitializeComponent();
         }
  
 
-        private void Drag_Form(object sender, MouseButtonEventArgs e)
+ 
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            DragMove();
+         
+            canvas.Children.Clear();
+
+             
+            double graphWidth = 400;
+            double graphHeight = 300;
+
+            
+            int maxValue = 0;
+            foreach (int dataPoint in dataPoints)
+            {
+                if (dataPoint > maxValue)
+                    maxValue = dataPoint;
+            }
+
+             
+            double barWidth = graphWidth / dataPoints.Length;
+
+           
+            for (int i = 0; i < dataPoints.Length; i++)
+            {
+                double barHeight = dataPoints[i] * graphHeight / maxValue;
+
+                Rectangle bar = new Rectangle
+                {
+                    Width = barWidth,
+                    Height = barHeight,
+                    Fill = Brushes.Blue
+                };
+
+                Canvas.SetLeft(bar, i * barWidth);
+                Canvas.SetTop(bar, graphHeight - barHeight);
+
+                canvas.Children.Add(bar);
+            }
         }
+
+
+
     }
 }
---
+++ GUI/StringExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    public static class StringExtension
+    {
+        public static (int, int) GetDevicePoint(this string value)
+        {
+            string[] points = value.Split('=')[1].Split(",");
+            int x = int.Parse(points[0]);
+            int y = int.Parse(points[1]);
+            return (x, y);
+        }
+
+        public static int GetDeviceValue(this string value)
+        {
+            int val = -1;
+            //Todo
+            // -100 = format exception
+            // -101 = undefined
+
+            string[] valueSplited = value.Split(',');
+            if (valueSplited.Length == 2)
+            {
+                try
+                {
+                    val = int.Parse(valueSplited[1]);
+                    return val;
+                }
+                finally
+                {
+                    val = -100;
+
+                }
+            }
+            return -1;
+        } 
+    }
+}
