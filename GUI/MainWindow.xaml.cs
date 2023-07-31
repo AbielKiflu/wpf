0f08bf1239e67846858275dc9035bd6ed5531c84
Abiel Kiflu <abiel.kiflu.weld@gmail.com>
2023-07-31 05:58:55 +0200

    Eventroute

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI.Views.Components;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,16 +21,24 @@ namespace GUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int[] dataPoints = { 3, 5, 8, 4, 7, 2 };
+        //private int[] dataPoints = { 3, 5, 8, 4, 7, 2 };
         public MainWindow()
         {
             InitializeComponent();
+          
         }
  
+        protected void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            lstPort.Items.Clear();
+            lstPort.Items.Add("Com1");
+            lstPort.Items.Add("Com2");
+           
+        }
 
- 
 
-  
+
+
 
 
     }
